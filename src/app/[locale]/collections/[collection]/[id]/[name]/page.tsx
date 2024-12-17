a0a6d5d3461258b0affe3e98b8d2558eb85080d4
Huseyin Onal <65453275+huseyinonalcom@users.noreply.github.com>
2024-12-17 16:42:10 +0300

    d

--- src/app/[locale]/collections/[collection]/[id]/[name]/page.tsx
+++ src/app/[locale]/collections/[collection]/[id]/[name]/page.tsx
@@ -1,6 +1,7 @@
 import { getTranslations } from "next-intl/server";
 import { collections } from "@/app/[locale]/page";
 import { notFound, permanentRedirect } from "next/navigation";
+import Image from "next/image";
 
 const getProduct = ({ param }: { param: { locale?: string; collection?: string; id?: string; name?: string } }) => {
   let allProducts = collections.flatMap((collection) =>
@@ -25,7 +26,7 @@ const getProduct = ({ param }: { param: { locale?: string; collection?: string; 
 };
 
 export default async function Home({ params }: { params: Promise<{ locale?: string; collection?: string; id?: string; name?: string }> }) {
-  const t = await getTranslations("home");
+  const t = await getTranslations("product");
   const param = await params;
 
   const product = getProduct({
@@ -36,7 +37,16 @@ export default async function Home({ params }: { params: Promise<{ locale?: stri
 
   return (
     <>
-      <div className="bg-red-500">a</div>
+      <div className="w-full flex flex-row items-center max-w-screen-xl px-4 mx-auto mt-12">
+        <div className="flex flex-col items-center gap-2 w-[180px]">
+          {product.files.map((file, i) => (
+            <div key={i} className="relative w-[150px] h-[150px]">
+              <Image fill key={i} sizes="150px" src={file.url} alt={"image for " + product.name} style={{ objectFit: "contain" }} />
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-row overflow-scroll no-scrollbar"></div>
+      </div>
     </>
   );
 }
